0ed7458056811551d1e0ad1c1ca04a22e191e7fc
user <user@es302n13>
2017-04-11 16:31:50 +0800

    0411 static

---
+++ src/oo/stati/Student.java
@@ -0,0 +1,29 @@
+package oo.stati;
+
+public class Student {
+
+	int english;
+	int math;
+	int chinese;
+	static int pass = 60;  //ÀRºA¡A¦@¥Îpass	
+
+//¥kÁä¡÷Source¡÷Generate Constructor
+	public Student(int english, int math, int chinese) {
+		super();
+		this.english = english;
+		this.math = math;
+		this.chinese = chinese;
+	}
+	
+	private String getScore(int score){
+		if (score < pass) {
+			return String.valueOf(score) + "*";
+		}else{
+			return String.valueOf(score);
+		}
+	}
+	
+	public void print(){
+		System.out.println(getScore(english) + "\t" + getScore(math) + "\t" + getScore(chinese));
+	}
+}
